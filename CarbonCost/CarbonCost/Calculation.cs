1c33d3df6aa08ac445b8ae38a3941703e8f2a29c
adzkiakhns <adzkiakhansameara@gmail.com>
2024-11-17 16:37:45 -0800

    ui added

---
+++ CarbonCost/CarbonCost/Calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarbonCost {
+    public class CarbonCreditCapCalculator
+    {
+        private double energyConsumption;
+        private double transportationHabits;
+        private double productionMetrics;
+
+
+        public double EnergyConsumption
+        {
+            get { return energyConsumption; }
+            set { energyConsumption = value; }
+        }
+
+        public double TransportationHabits
+        {
+            get { return transportationHabits; }
+            set { transportationHabits = value; }
+        }
+
+        public double ProductionMetrics
+        {
+            get { return productionMetrics; }
+            set { productionMetrics = value; }
+        }
+
+        public double CalculationCap()
+        {
+            double cap = 0.0;
+
+            cap = (energyConsumption + transportationHabits + ProductionMetrics) / 3;
+            return cap;
+        }
+
+    }
+}
